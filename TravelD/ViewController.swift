92c660a1956ebf49346b1029485efd1c9a12f24f
Arailym <asermagambet@mail.ru>
2024-05-24 01:08:21 +0500

    Initial Commit

---
+++ TravelD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelD
+//
+//  Created by Арайлым Сермагамбет on 24.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
